7e23f39f642d284dbcfae2f5581a94bca958d45f
artskin <artskin@163.com>
2018-03-22 23:41:46 +0800

    add mp3

--- main.js
+++ main.js
@@ -1,74 +1,127 @@
 /*
  * @name: 大鱼吃小鱼
- * @author: artskin(artskin@163.com)
+ * @author: 沐峰(artskin@163.com)
  * @date: 2018-3-20
  *
  */
+
 (function(){
     var DPR = window.devicePixelRatio,
-        gameW = document.body.clientWidth *DPR,
-        gameH = document.body.clientHeight *DPR,
-        fontSize = 28*DPR;
+        gameW = document.body.clientWidth * DPR,
+        gameH = document.body.clientHeight * DPR,
+        fontSize = 16*DPR;
 
     var game = new Phaser.Game(gameW,gameH,Phaser.CANVAS,'game');
 
     var loading,progress,timer,ball;
     var enemy,player,dragArea,sBall,ballNum;
-    var button2,scoreText,titleTxt;
+    var button2,scoreText,overTxt,progressBar;
     var score = 0;
-    console.log(game)
+    var unit = 1080 / gameW;
 
+    //绘制球形
+    function ShapeBall(color1,color2,size) {
+        this.bmd = game.make.bitmapData(size*2, size*2);
+        this.ballStyle = this.bmd.context.createRadialGradient(
+            size*0.6,
+            size*0.6,
+            size*0.1,
+            size,
+            size,
+            size * 1.2
+        );
+        this.ballStyle.addColorStop(0, color1);
+        this.ballStyle.addColorStop(1, color2);
+        this.bmd.circle(size, size, size, this.ballStyle);
+
+        return this.bmd;
+    }
+
+    //生成随机颜色
+    function RandomColor() {
+        this.highlight = 88;
+        this.r=Math.floor(Math.random()*256);
+        this.g=Math.floor(Math.random()*(256-this.highlight));
+        this.b=Math.floor(Math.random()*256);
+        this.g2 = this.g + this.highlight;
+        //所有方法的拼接都可以用ES6新特性`其他字符串{$变量名}`替换
+        return{
+            color1:"#" + ((1 << 24) + (this.r << 16) + (this.g << 8) + this.b).toString(16).slice(1),
+            color2:"#" + ((1 << 24) + (this.r << 16) + (this.g2 << 8) + this.b).toString(16).slice(1)
+        }
+    }
 
     //游戏加载
     var bootState = function (game) {
-        this.preload = function(){
-            game.load.image("loading","./assets/loading.gif")
-        };
-        this.create =function(){
-            console.log("loading");
-            var style = {font:"36px",fill:"#fff"};
-            var gameTitle = game.add.text(0,game.height/3,'loading',style);
-            gameTitle.x = (game.width - gameTitle.width)/2;
-            gameTitle.alpha =1;
+        this.init = function () {
+            //缩放设置
             game.scale.scaleMode = Phaser.ScaleManager.EXACT_FIT;
             game.stage.backgroundColor = "#1b2436";
-            game.state.start('start');
+        };
+        this.preload = function(){
+            //资源加载
+            game.load.image('playerArea', './assets/opa.png');
+            game.load.image("loading","./assets/loading.gif");
+            game.load.image('progressBar', './assets/progressBar.png');
+            game.load.image('title', './assets/title.png');
+            game.load.spritesheet('button', './assets/button_sprite_sheet.png', 361, 124);
+            game.load.spritesheet('button2', './assets/button_sprite_sheet.png', 361, 118);
+            game.load.audio('bgm', './assets/audio/bgm.mp3', true);
+            game.load.audio('get', './assets/audio/get.mp3', true);
+            game.load.audio('boom', './assets/audio/boom.mp3', true);
+
+            game.load.onLoadComplete.add(function () {
+                game.sound.setDecodedCallback(['bgm','get','boom'],function () {
+                    game.add.tween(progressBar).to({width:304/unit},600,null,true);
+                    setTimeout(function () {
+                        game.state.start('start');
+                    }, 1200);
+                },this);
+            });
+        };
+        this.create =function(){
+
+            var loadStyle = {font:fontSize+"px",fill:"#fff"};
+            var loadText = game.add.text(gameW/2,gameH/2-60,'loading',loadStyle);
+            loadText.anchor.setTo(0.5, 0.5);
+            loadText.alpha =0.6;
+
+            loading = game.add.sprite(gameW/2,gameH/2,"loading");
+            loading.anchor.setTo(0.5, 0.5);
+
+            progressBar = game.add.sprite(gameW/2 - 104, gameH/2, 'progressBar');
+            progressBar.anchor.set(0, 0.5);
+            progressBar.width = 0/unit;
+            progressBar.height = 20 / unit;
         };
     };
-    console.log(bootState);
 
     //游戏开始画面
     var startState = function (game) {
         var bmd,colors,rectangle,ball,getColor,innerCircle,outerCircle,rgb,button,title;
-        var i = 0;
-        var p =null;
 
-        this.preload =function(){
-            game.load.spritesheet('button', './assets/button_sprite_sheet.png', 361, 124);
-            game.load.spritesheet('title', './assets/title.png', 700, 0);
-        };
         this.create = function(){
-            console.log("开始画面");
+            console.log("开始游戏");
             //游戏名称
-            title = game.add.sprite(game.width/5+28,game.world.height/5-50,'title');
+            title = game.add.sprite(gameW/2,gameH/5-50,'title');
+            title.anchor.setTo(0.5, 0.5);
             title.scale.setTo(0.3*DPR);
 
             var style = {font:fontSize/2+"px",fill:"#394e76"};
-            var gameTitle = game.add.text(0,game.height-100,'小提示：吃掉比你小的球',style);
-            gameTitle.x = (game.width - gameTitle.width)/2;
-            gameTitle.alpha =1;
+            var gameTips = game.add.text(gameW/2,gameH-100,'小提示：吃掉比你小的球',style);
+            gameTips.anchor.setTo(0.5, 0.5);
+            gameTips.alpha =1;
 
             //启动动画
-            //var circle = new ShapeBall(colorObj.color2,colorObj.color1,size);
             var circle1 = new ShapeBall("#8ED6FF","#003BA2",60*DPR);
             var circle2 = new ShapeBall("#94ff6f","#2aa200",20*DPR);
 
-            player = game.add.sprite(game.width/2+40*DPR,game.height/2,circle2);
-            ball = game.add.sprite(game.width/2 -80*DPR,game.height/4+100,circle1);
+            player = game.add.sprite(gameW/2+40*DPR,gameH/2,circle2);
+            ball = game.add.sprite(gameW/2 -80*DPR,gameH/4+100,circle1);
 
             game.add.tween(ball).to(
                 {
-                    y: game.height / 2 - 50*DPR,
+                    y: gameH / 2 - 50*DPR,
                     radius: 1
                 },
                 1000,
@@ -78,42 +131,38 @@
             );
 
             //开始按钮
-            button = game.add.button(game.world.centerX - 172, game.height*0.72, 'button', actionOnClick, this, 2, 1, 0);
+            button = game.add.button(gameW/2, gameH*0.72, 'button', actionOnClick, this, 2, 1, 0);
+            button.anchor.setTo(0.5, 0.5);
             function actionOnClick () {
                 game.state.start('main');
             }
 
         };
         this.update = function(){
-            //bmd.setPixel(p.x, p.y, colors[i].r, colors[i].g, colors[i].b);
+
         };
         this.render = function () {
             //game.debug.geom(ball,'#cfffff');
-
         }
     };
 
     var mainState = function (game) {
-        this.preload = function(){
-            game.load.image('playerArea', './assets/opa.png');
-        };
         this.addBall = function () {
             var colorObj = new RandomColor();
             var size = Math.floor(Math.random()*10 +1) * 5*DPR;
-
             if(size > 90){
                 size = size -80;
             }
-
             var circle = new ShapeBall(colorObj.color2,colorObj.color1,size);
-            var ballX = game.world.randomX;
-            var ballY = game.world.randomY;
-            if(ballX < game.width/2){
+
+            var ballX = game.world.randomX,
+                ballY = game.world.randomY;
+            if(ballX < gameW/2){
                 ballX = -ballX;
             }else{
-                ballX = ballX+game.width;
+                ballX = ballX+gameW;
             }
-            if(ballY < game.height/2){
+            if(ballY < gameH/2){
                 ballY = -ballY;
             }else{
                 ballY = ballY;
@@ -123,9 +172,8 @@
             sBall.body.collideWorldBounds = true;
             sBall.body.setCircle(size/2);
             sBall.body.bounce.set(1);
-            sBall.body.gravity.set(0,2);
-            console.log((150 - size)*DPR,(100 - size)*DPR);
-            sBall.body.velocity.set((150 - size)*DPR,(100 - size)*DPR);
+            sBall.body.gravity.set(0,0);
+            sBall.body.velocity.set((180 - size)*DPR,(140 - size)*DPR);
             //sBall.body.angularVelocity = size;
             return ballNum = enemy.children.length
 
@@ -140,42 +188,35 @@
             enemy.enableBody = true;
             enemy.physicsBodyType = Phaser.Physics.ARCADE;
 
-            for(var c=0;c<5;c++) {
+            for(var c=0;c<4;c++) {
                 this.addBall();
             }
-            //console.log(this.addBall());
 
             //玩家设置
             var Shape = new ShapeBall("#ff5757","#aa0101",11*DPR);
-            player = game.add.sprite(game.width/2 -Shape.width/2,game.height/2-Shape.height/2,Shape);
+            player = game.add.sprite(gameW/2 -Shape.width/2,gameH/2-Shape.height/2,Shape);
 
             //拖动设置
-            dragArea = game.add.sprite(-game.width/2,-game.height/2, 'playerArea');
-            //dragArea = game.add.sprite(0,0, 'playerArea');
-            dragArea.width = game.width*2;
-            dragArea.height = game.height*2;
+            dragArea = game.add.sprite(-gameW/2,-gameH/2, 'playerArea');
+            dragArea.width = gameW*2;
+            dragArea.height = gameH*2;
             dragArea.inputEnabled = true; //sprite to input
             dragArea.input.enableDrag(); //input set drag
-            //dragArea.hitArea = new Phaser.Circle(game.width / 2, game.height / 2, 90); //input set drag
+            //dragArea.hitArea = new Phaser.Circle(gameW / 2, gameH / 2, 90); //input set drag
 
             dragArea.events.onDragStart.add(dragStart);
             dragArea.events.onDragUpdate.add(dragUpdate);
             //dragArea.events.onDragStop.add(dragStop);
 
             function dragStart(e) {
-                // return {
-                //     moveX:e.position.x*DPR - player.position.x,
-                //     moveY:e.position.y*DPR - player.position.y
-                // };
-                player.position.x = game.width/DPR + e.position.x +88*DPR*DPR;
-                player.position.y = game.height/DPR + e.position.y +161*DPR*DPR;
+                return {
+                    moveX:gameW/DPR + e.position.x +88*DPR*DPR,
+                    moveY:gameH/DPR + e.position.y +161*DPR*DPR
+                };
             }
             function dragUpdate(e) {
-                //console.log(player.position.x);
-                // player.position.x = player.position.x/2+e.position.x/2 +88*DPR+game.width/4;
-                // player.position.y = player.position.y/2+e.position.y/2 +161*DPR+game.height/4;
-                player.position.x = game.width/DPR + e.position.x +88*DPR*DPR;
-                player.position.y = game.height/DPR + e.position.y +161*DPR*DPR;
+                player.position.x = dragStart(e).moveX;
+                player.position.y = dragStart(e).moveY;
             }
 
             function dragStop() {
@@ -183,9 +224,9 @@
             }
 
             //统计得分
-            scoreText = game.add.text(24,24,'分数：0', { fontSize: '36px', fill: '#fff' });
+            scoreText = game.add.text(24,24,'分数：0', { fontSize: fontSize+'px', fill: '#fff' });
 
-            game.time.events.loop(2000/DPR, this.addBall, this);
+            game.time.events.loop(1800/DPR, this.addBall, this);
             //game.add.tween(player).to( { angle: 360 }, 2000, Phaser.Easing.Linear.None, true);
             game.physics.arcade.enable([enemy,player], Phaser.Physics.ARCADE);
 
@@ -231,12 +272,7 @@
     };
 
     var endState = function (game) {
-
-        this.preload = function () {
-            game.load.spritesheet('button', './assets/button_sprite_sheet.png', 361, 118);
-        };
         this.create = function(){
-
             function toPercent(point){
                 var str=Number(point*100).toFixed(1);
                 if (str>100){
@@ -245,26 +281,26 @@
                 str+="%";
                 return str;
             }
-            var per = toPercent(score/211);
+            var per = toPercent(score/412);
 
             //结语
-            titleTxt =  game.add.text(0 ,game.height/5,'Game Over!', { fontSize: fontSize*1.2+'px', fill: '#c00',boundsAlignH:'center' });
-            titleTxt.setTextBounds(0, 100, game.width, 100);
+            overTxt =  game.add.text(gameW/2 ,gameH/5,'Game Over!', { fontSize: fontSize*2+'px', fill: '#c00',boundsAlignH:'center' });
+            overTxt.anchor.setTo(0.5, 0.5);
 
-            scoreText = game.add.text(0 ,game.height/3,'我的战绩：'+score, { fontSize: '36px', fill: '#fff',boundsAlignH:'center' });
-            scoreText.setTextBounds(0, 100, game.width, 100);
-            scoreText = game.add.text(0 ,game.height/3+65,"击败了全国"+per+"的人", { fontSize: '36px', fill: '#fff',boundsAlignH:'center' });
-            scoreText.setTextBounds(0, 100, game.width, 100);
+            scoreText = game.add.text(gameW/2 ,gameH/3,'我的战绩：'+score, { fontSize: fontSize+'px', fill: '#fff',boundsAlignH:'center' });
+            scoreText.anchor.setTo(0.5, 0.5);
+            scoreText = game.add.text(gameW/2 ,gameH/3+fontSize+30,"击败了全国"+per+"的选手", { fontSize: fontSize+'px', fill: '#fff',boundsAlignH:'center' });
+            scoreText.anchor.setTo(0.5, 0.5);
 
             //重新开始
-            button2 = game.add.button(game.world.centerX - 172, game.height*0.72, 'button', actionOnClick, this, 2, 1, 0);
+            button2 = game.add.button(gameW/2, gameH*0.72, 'button2', actionOnClick, this, 2, 1, 0);
+            button2.anchor.setTo(0.5, 0.5);
             function actionOnClick () {
                 score = 0;
                 game.state.start('start');
             }
         };
     };
-
 
     game.state.add('boot',bootState);
     game.state.add('start',startState);
@@ -273,38 +309,6 @@
 
     //游戏初始化
     game.state.start('boot');
-
-    //绘制球形
-    function ShapeBall(color1,color2,size) {
-        this.bmd = game.make.bitmapData(size*2, size*2);
-        this.ballStyle = this.bmd.context.createRadialGradient(
-            size*0.6,
-            size*0.6,
-            size*0.1,
-            size,
-            size,
-            size * 1.2
-        );
-        this.ballStyle.addColorStop(0, color1);
-        this.ballStyle.addColorStop(1, color2);
-        this.bmd.circle(size, size, size, this.ballStyle);
-
-        return this.bmd;
-    }
-
-    //生成随机颜色
-    function RandomColor() {
-        this.highlight = 88;
-        this.r=Math.floor(Math.random()*256);
-        this.g=Math.floor(Math.random()*(256-this.highlight));
-        this.b=Math.floor(Math.random()*256);
-        this.g2 = this.g + this.highlight;
-        //所有方法的拼接都可以用ES6新特性`其他字符串{$变量名}`替换
-        return{
-            color1:"#" + ((1 << 24) + (this.r << 16) + (this.g << 8) + this.b).toString(16).slice(1),
-            color2:"#" + ((1 << 24) + (this.r << 16) + (this.g2 << 8) + this.b).toString(16).slice(1)
-        }
-    }
 
 })();
 
